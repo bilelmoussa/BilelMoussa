ef666aca19480d12ea77c9fbce7ad3765a7e1202
bilelmoussa <streetvayne@outlook.fr>
2019-11-01 00:23:27 +0100

    .

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 let secret = process.env.SECRET || "SECRET";
-let Mongodb_key = "mongodb+srv://bilel123:Azertyub1_@cluster0-yr3qy.mongodb.net/test?retryWrites=true&w=majority";
+let Mongodb_key = "mongodb+srv://bilel123:Azertyub1_@3dwave-xwprz.mongodb.net/test?retryWrites=true";
 
 module.exports = {
     database: Mongodb_key,
